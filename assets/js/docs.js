07b229a72504c02c63fca416726e9e6fd576b7e8
Raveena-aot <raveena.r@aot-technologies.com>
2022-11-09 11:43:24 +0530

    updated quickinstallation

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -82,6 +82,29 @@ sidebarLinks.forEach((sidebarLink) => {
   });
 });
 
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
 /* ===== Gumshoe SrollSpy ===== */
 /* Ref: https://github.com/cferdinandi/gumshoe  */
 // Initialize Gumshoe

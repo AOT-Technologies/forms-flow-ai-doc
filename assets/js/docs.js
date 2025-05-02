f076ba515dd2105888edb4e23c3ddfc03d5f28e5
msergeyenko-aot <max.sergeyenko@aot-technologies.com>
2025-05-02 09:57:38 -0700

    Update docs to new FormFlow (#70)
    * Update content 1
    
    * Update intro
    
    * Update Examples 1
    
    * Examples update 2
    
    * Update Examples 3
    
    * Update screenshots
    
    * Update screenshots 2
    
    * Update RPA page
    
    * Update image classes on about, architecture, introduction
    
    * Update introduction.html
    
    * Update role image classes
    
    * Update language image class
    
    * Add max-width fit content to new img classes
    
    * Update anonymous forms classes
    
    * Update image classes on half of features pages
    
    * Some more class updates for images
    
    * Update image classes 2
    
    * Update anonymous form
    
    * Update anonymous_form.html
    
    * Draft Feature updated content
    
    * Update draft_feature.html
    
    * Update Export PDF
    
    * Update exportpdf.html
    
    * Update exportpdf.html
    
    * Update Task Filters content
    
    * Update Task Variables content
    
    * Update task_variables.html
    
    * Update task_variables.html
    
    * Update task_variables.html
    
    * Update Analytics User Guide content
    
    * Update redash_guide.html
    
    * Update RBA content
    
    * Update rbac.html
    
    * Update rbac.html
    
    * Admin content updates
    
    * Update Flow-E page
    
    * Update chatbot and integrations
    
    * Finish updating integrations page
    
    * Update iPaas.html
    
    * Update Bundle page
    
    * Adjust typos in Bundle
    
    * Test Menu update
    
    * Rename Main Menu items
    
    * Update index.html
    
    * Add premium label to premium features
    
    * Add No-Code page
    
    * Edit No-Code content
    
    * No-content added
    
    * Update No-Code further
    
    * Update No-Code content
    
    * Update Template content
    
    * Update templates content #2
    
    * Add Permissions page
    
    * Update permissions.html
    
    * Update permissions.html
    
    * Update name for permissions page
    
    * Update exportpdf.html
    
    * Update Main Menu structure #2
    
    * Update Download button on top
    
    * Hide Download and install section
    
    * Start on expanded main menu
    
    * Update docs.js
    
    * Update index.html
    
    * Minor Adjustments
    
    * Update index.html
    
    * Update svg-with-js.min.css
    
    * Update theme.css
    
    * Update index.html
    
    * Update menu label
    
    * Update Supporting Browsers
    
    * Update supportingbrowsers.html
    
    * Get rid of padding on the bottom
    
    * Prev button adjust
    
    * Add roles to usage
    
    * Further update to Usage
    
    * Update examples.html
    
    * Fixed Flow-E title
    
    * Adding changes for migration path
    
    * Adding architecture diagram
    
    * Update architecture.html
    
    * Update version_upgrade.html
    
    * Adjust content as per Vinod's feedback
    
    * Update content as per Vinod's feedback 2
    
    * Update headings and page file names for easier search
    
    * Update page names 2
    
    * Update content based on Vinod feedback 3
    
    * Stop nav from collapsing on click
    
    * Stop menu collapsing 2
    
    * Adding Email and Custom theme
    
    * Changes
    
    * Update custom_theme.html
    
    * Update BPMN Listeners content as per Vinod
    
    * BPMN content changes from Vinod
    
    * Make templates clickable
    
    * Update case on Custom Theme
    
    * Make templates clickable 2
    
    * Change link on Embed
    
    * Move Custom theme alphabetically
    
    ---------
    
    Co-authored-by: Sumesh Kariyil <sumesh.pk@aot-technologies.com>

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -11,7 +11,8 @@ window.onload = function () {
   let url = (window.location).href
   const id = url.substring(url.lastIndexOf("#")+1,url.length)
   responsiveSidebar();
-  $("li:not(.section-title):not(.intro)").hide()
+  // hide main menu sections on load
+  // $("li:not(.section-title):not(.intro)").hide()
 
   const itemExist = document.getElementById(id)
   let target = itemExist? id : "introduction";
